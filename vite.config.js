4e2baade1f9ae59957a2bcc80344ed4f4b074d2d
Neha Verma <nehavjay15@gmail.com>
2026-04-24 14:38:54 +0530

    added robot and llm file for seo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://shopfiesta.vercel.app',
+      dynamicRoutes: [
+        '/',
+        '/products',
+        '/about',
+        '/support',
+        '/shipping-returns',
+        // You can fetch and map dynamic product IDs here later
+      ]
+    })
+  ],
 })
